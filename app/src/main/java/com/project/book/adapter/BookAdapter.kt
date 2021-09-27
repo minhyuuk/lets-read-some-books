79261fe17ce139303408b1df3a54b160443142dd
정민혁 <jlssteeve1@gmail.com>
2021-09-27 20:58:15 +0900

    [ADD] glide in bind

--- app/src/main/java/com/project/book/adapter/BookAdapter.kt
+++ app/src/main/java/com/project/book/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.project.book.databinding.ItemBookBinding
 import com.project.book.model.Book
 
@@ -15,6 +16,12 @@ class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) 
 
         fun bind(bookModel: Book) {
             binding.titleTextView.text = bookModel.title
+            binding.descriptionTextView.text = bookModel.description
+
+            Glide
+                .with(binding.coverImageView.context)
+                .load(bookModel.coverSmallUrl)
+                .into(binding.coverImageView)
         }
     }
 
