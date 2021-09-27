8b3f77120edc83c4ed326deabfb76305aef669d4
정민혁 <jlssteeve1@gmail.com>
2021-09-27 20:01:53 +0900

    [FIX] SerializedName

---
+++ app/src/main/java/com/project/book/util/Constants.kt
@@ -0,0 +1,19 @@
+package com.project.book.util
+
+object Constants {
+    const val TAG:String = "로"
+}
+
+enum class SEARCH_TYPE{
+    USER
+}
+
+enum class RESPONSE_STATE{
+    OKAY,
+    FAIL
+}
+object API {
+    const val BASE_URL : String = "https://book.interpark.com"
+    const val SEARCH_WELFARE : String = ""
+    const val KEY : String = "162DC33D7A1AC38FC0B10A94587A93407BDA8368C523670B3BCB07014E1C183B"
+}
