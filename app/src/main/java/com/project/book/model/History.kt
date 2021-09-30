9b4eb5801f56f5c62b2f01731a89d813a7150d46
정민혁 <jlssteeve1@gmail.com>
2021-09-30 18:05:05 +0900

    [CREATE] history entity

---
+++ app/src/main/java/com/project/book/model/History.kt
@@ -0,0 +1,12 @@
+package com.project.book.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class History(
+    @PrimaryKey val uid: Int?,
+    @ColumnInfo(name = "keyword") val keyword: String?
+)
+
