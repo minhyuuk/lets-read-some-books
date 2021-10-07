202a9103b30b1d410592e0acaf6dbd5faa126abb
정민혁 <jlssteeve1@gmail.com>
2021-10-07 23:46:07 +0900

    [FIX] API key

--- app/src/main/java/com/project/book/util/Constants.kt
+++ app/src/main/java/com/project/book/util/Constants.kt
@@ -1,19 +1,6 @@
 package com.project.book.util
 
-object Constants {
-    const val TAG:String = "로"
-}
-
-enum class SEARCH_TYPE{
-    USER
-}
-
-enum class RESPONSE_STATE{
-    OKAY,
-    FAIL
-}
 object API {
-    const val BASE_URL : String = "https://book.interpark.com"
-    const val SEARCH_WELFARE : String = ""
-    const val KEY : String = "162DC33D7A1AC38FC0B10A94587A93407BDA8368C523670B3BCB07014E1C183B"
+    const val BASE_URL : String = "https://book.interpark.com/api/"
+    const val KEY : String = "765A3F027389533B4C069DA9B269D806EDDB5CC1431CD8A053BB6FA19D6C1642"
 }
