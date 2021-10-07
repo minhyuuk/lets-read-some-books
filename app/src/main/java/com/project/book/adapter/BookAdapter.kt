82880fda4479a961c568f662c554f1359e8373ef
정민혁 <jlssteeve1@gmail.com>
2021-10-07 23:45:50 +0900

    [ADD] annotation glide module

--- app/src/main/java/com/project/book/adapter/BookAdapter.kt
+++ app/src/main/java/com/project/book/adapter/BookAdapter.kt
@@ -6,11 +6,13 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
 import com.project.book.databinding.ItemBookBinding
 import com.project.book.model.Book
 
 class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
 
+    @GlideModule
     inner class BookItemViewHolder(private val binding: ItemBookBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
