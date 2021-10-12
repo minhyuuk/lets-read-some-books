8c7854027f0dee46c5a565eaed19028b471b0bfa
정민혁 <jlssteeve1@gmail.com>
2021-10-12 22:53:31 +0900

    [ADD] new database

--- app/src/main/java/com/project/book/AppDatabase.kt
+++ app/src/main/java/com/project/book/AppDatabase.kt
@@ -3,10 +3,12 @@ package com.project.book
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.project.book.dao.HistoryDao
+import com.project.book.dao.ReviewDao
 import com.project.book.model.History
+import com.project.book.model.Review
 
-@Database(entities = [History::class],version = 1)
+@Database(entities = [History::class, Review::class],version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun historyDao(): HistoryDao
-
+    abstract fun reviewDao(): ReviewDao
 }
