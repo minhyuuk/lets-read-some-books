82fce4599fa85a2a1b91b5f26d546cdcdfde1bda
정민혁 <jlssteeve1@gmail.com>
2021-10-19 23:28:37 +0900

    [ADD] splash actvitiy

---
+++ app/src/main/java/com/project/book/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.project.book.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.book.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
