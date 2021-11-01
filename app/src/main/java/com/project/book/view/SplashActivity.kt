9b94c4c5c3e52834b5218e08f362bc8e6ff5d6c0
정민혁 <jlssteeve1@gmail.com>
2021-11-01 23:58:01 +0900

    [CREATE] motionlayout with splash

--- app/src/main/java/com/project/book/view/SplashActivity.kt
+++ app/src/main/java/com/project/book/view/SplashActivity.kt
@@ -1,23 +1,76 @@
 package com.project.book.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.project.book.R
+import com.project.book.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+        handler()
     }
+
+    private fun handler(){
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent)
+        }, DURATION)
+    }
+
+    private fun nextActivity(){
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object :MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+    }
+
+
+
+    companion object {
+        private const val DURATION : Long = 1600
+    }
+
 }
-//
-//<scale xmlns:android="http://schemas.android.com/apk/res/android"
-//android:duration="1000"
-//android:fromXScale="0"
-//android:fromYScale="0"
-//android:pivotX="50%"
-//android:pivotY="50%"
-//android:toXScale="0.5"
-//android:toYScale="0.5">
-//
-//</scale>
