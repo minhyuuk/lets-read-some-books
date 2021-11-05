62786e132760170dc67e5f49d126b243a251dd3c
정민혁 <jlssteeve1@gmail.com>
2021-11-05 16:18:24 +0900

    [FIX] create func

--- app/src/main/java/com/project/book/view/MainActivity.kt
+++ app/src/main/java/com/project/book/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.project.book.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
 import androidx.core.view.isVisible
@@ -26,115 +28,139 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: BookAdapter
+    private lateinit var bookRecyclerViewAdapter: BookAdapter
+    private lateinit var bookService: BookService
     private lateinit var historyAdapter: HistoryAdapter
 
-    private lateinit var service: BookService
-
-    private lateinit var db: AppDatabase
+    private val db: AppDatabase by lazy {
+        getAppDatabase(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initBookRecyclerView()
+        initHistoryRecyclerView()
+        initSearchEditText()
 
-        db = getAppDatabase(this@MainActivity)
+        initBookService()
+        bookServiceLoadBestSellers()
+    }
 
-        adapter = BookAdapter(clickListener = {
+    private fun bookServiceLoadBestSellers() {
+        // 베스트 셀러 가져오기;
+        bookService.getBestSeller(API.KEY)
+            .enqueue(object : Callback<BestSellerDTO> {
+                // 응답이 온 경우;
+                override fun onResponse(
+                    call: Call<BestSellerDTO>,
+                    response: Response<BestSellerDTO>
+                ) {
+                    // 받은 응답이 성공한 응답일 때;
+                    if (response.isSuccessful.not()) {
+                        Log.e(M_TAG, "NOT!! SUCCESS")
+                        return
+                    }
+
+                    // 받은 응답의 바디가 채워져 있는 경우만 진행;
+                    response.body()?.let {
+                        Log.d(M_TAG, it.toString())
+
+                        it.books.forEach { book ->
+                            Log.d(M_TAG, book.toString())
+                        }
+
+                        // 새 리스트로 갱신;
+                        bookRecyclerViewAdapter.submitList(it.books)
+                    }
+                }
+
+                // 응답에 실패한 경우
+                override fun onFailure(call: Call<BestSellerDTO>, t: Throwable) {
+                    Log.e(M_TAG, t.toString())
+                }
+            })
+    }
+
+    private fun initBookService() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(API.BASE_URL) // 인터파크 베이스 주소;
+            .addConverterFactory(GsonConverterFactory.create()) // Gson 변환기 사용;
+            .build()
+
+        bookService = retrofit.create(BookService::class.java)
+    }
+
+    private fun initBookRecyclerView() {
+        bookRecyclerViewAdapter = BookAdapter(clickListener = {
             val intent = Intent(this, DetailActivity::class.java)
+
+            // 직렬화 해서 넘길 것.
             intent.putExtra("bookModel", it)
             startActivity(intent)
         })
 
-        historyAdapter = HistoryAdapter(historyDeleteClickListener = {
-            deleteSearchKeyword(it)
-        })
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(API.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        service = retrofit.create(BookService::class.java)
-        service.getBestSeller(API.KEY)
-            .enqueue(object: Callback<BestSellerDTO> {
-                override fun onFailure(call: Call<BestSellerDTO>, t: Throwable) {
-
-                }
-
-                override fun onResponse(call: Call<BestSellerDTO>, response: Response<BestSellerDTO>) {
-                    if (response.isSuccessful.not()) {
-                        return
-                    }
-
-                    response.body()?.let {
-                        adapter.submitList(it.books)
-                    }
-                }
-            })
-
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = adapter
-
-        binding.searchEditText.setOnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
-                search(binding.searchEditText.text.toString())
-                return@setOnKeyListener true
-            }
-            return@setOnKeyListener false
-
-        }
-
-        binding.searchEditText.setOnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                showHistoryView()
-            }
-
-            return@setOnTouchListener false
-        }
-
-
-        binding.historyRecyclerView.adapter = historyAdapter
-        binding.historyRecyclerView.layoutManager = LinearLayoutManager(this)
-
-
+        binding.recyclerView.adapter = bookRecyclerViewAdapter
     }
 
-    private fun search(text: String) {
+    private fun initHistoryRecyclerView() {
+        historyAdapter = HistoryAdapter(historyDeleteClickListener = {
+            deleteSearchKeyword(it)
+        }, this)
 
-        service.getBooksByName(API.KEY, text)
-            .enqueue(object: Callback<SearchBooksDTO> {
-                override fun onFailure(call: Call<SearchBooksDTO>, t: Throwable) {
-                    hideHistoryView()
-                }
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.historyRecyclerView.adapter = historyAdapter
 
-                override fun onResponse(call: Call<SearchBooksDTO>, response: Response<SearchBooksDTO>) {
+        initSearchEditText()
+    }
+
+
+    fun bookServiceSearchBook(keyword: String) {
+
+        bookService.getBooksByName(API.KEY,keyword)
+            .enqueue(object : Callback<SearchBooksDTO> {
+                // 성공.
+
+                override fun onResponse(
+                    call: Call<SearchBooksDTO>,
+                    response: Response<SearchBooksDTO>
+                ) {
                     hideHistoryView()
-                    saveSearchKeyword(text)
+                    saveSearchKeyword(keyword)
 
                     if (response.isSuccessful.not()) {
                         return
                     }
 
-                    response.body()?.let {
-                        adapter.submitList(it.books)
-                    }
+                    bookRecyclerViewAdapter.submitList(response.body()?.books.orEmpty()) // 새 리스트로 갱신
                 }
 
+                // 실패.
+                override fun onFailure(call: Call<SearchBooksDTO>, t: Throwable) {
+                    hideHistoryView()
+                    Log.e(M_TAG, t.toString())
+                }
             })
+    }
+
+    private fun saveSearchKeyword(keyword: String) {
+        Thread {
+            db.historyDao().insertHistory(History(null, keyword))
+        }.start()
     }
 
     private fun showHistoryView() {
-        Thread(Runnable {
-            db.historyDao().getAll().reversed().run {
-                runOnUiThread {
-                    binding.historyRecyclerView.isVisible = true
-                    historyAdapter.submitList(this)
-                }
+        Thread {
+            val keywords = db.historyDao().getAll().reversed()
+            runOnUiThread {
+                binding.historyRecyclerView.isVisible = true
+                historyAdapter.submitList(keywords.orEmpty())
             }
-
-        }).start()
+        }.start()
 
     }
 
@@ -142,16 +168,35 @@ class MainActivity : AppCompatActivity() {
         binding.historyRecyclerView.isVisible = false
     }
 
-    private fun saveSearchKeyword(keyword: String) {
-        Thread(Runnable {
-            db.historyDao().insertHistory(History(null, keyword))
-        }).start()
-    }
-
     private fun deleteSearchKeyword(keyword: String) {
-        Thread(Runnable {
+        Thread {
             db.historyDao().delete(keyword)
             showHistoryView()
-        }).start()
+        }.start()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initSearchEditText() {
+        binding.searchEditText.setOnKeyListener { v, keyCode, event ->
+            // 키보드 입력시 발생
+
+            // 엔터 눌렀을 경우 (눌렀거나, 뗏을 때 -> 눌렀을 때 발생하도록.)
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                bookServiceSearchBook(binding.searchEditText.text.toString())
+                return@setOnKeyListener true// 처리 되었음.
+            }
+            return@setOnKeyListener false // 처리 안됬음 을 나타냄.
+        }
+
+        binding.searchEditText.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                showHistoryView()
+            }
+            return@setOnTouchListener false
+        }
+    }
+
+    companion object {
+        private const val M_TAG = "MainActivity"
     }
 }
