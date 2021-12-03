b7a65ba9bbc4b38e42730309246872500ea20bbd
정민혁 <jlssteeve1@gmail.com>
2021-12-03 15:23:49 +0900

    [ADD] toolbar custom

--- app/src/main/java/com/project/book/view/home/HomeFragment.kt
+++ app/src/main/java/com/project/book/view/home/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.project.book.view.home
 
+import android.content.Intent
+import androidx.appcompat.widget.Toolbar
 import com.project.book.R
 import com.project.book.base.BaseFragment
 import com.project.book.databinding.FragmentHomeBinding
+import com.project.book.view.main.MainActivity
+import com.project.book.view.search.SearchActivity
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
 
     override fun FragmentHomeBinding.onViewCreated() {
 
@@ -13,6 +18,5 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun FragmentHomeBinding.onCreateView() {
 
     }
-
 
 }
