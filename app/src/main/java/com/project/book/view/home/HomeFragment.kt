5753bb3a648319c409ed460fbaef961d1f92d1bf
정민혁 <jlssteeve1@gmail.com>
2021-12-16 23:35:12 +0900

    [ADD] 화면 이동

--- app/src/main/java/com/project/book/view/home/HomeFragment.kt
+++ app/src/main/java/com/project/book/view/home/HomeFragment.kt
@@ -1,22 +1,38 @@
 package com.project.book.view.home
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.widget.Toolbar
+import android.util.Log
 import com.project.book.R
 import com.project.book.base.BaseFragment
 import com.project.book.databinding.FragmentHomeBinding
-import com.project.book.view.main.MainActivity
+import com.project.book.view.profile.ProfileFragment
 import com.project.book.view.search.SearchActivity
+import com.project.book.view.settings.SettingsFragment
+import kotlin.concurrent.fixedRateTimer
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-
     override fun FragmentHomeBinding.onViewCreated() {
-
+        binding.profileImg.setOnClickListener {
+            fragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.fl_container,ProfileFragment())
+                ?.commit()
+        }
+        binding.searchImg.setOnClickListener {
+            val intent = Intent(activity,SearchActivity::class.java)
+            startActivity(intent)
+        }
+        binding.settingsImg.setOnClickListener {
+            fragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.fl_container,SettingsFragment())
+                ?.commit()
+        }
     }
 
     override fun FragmentHomeBinding.onCreateView() {
 
     }
-
 }
