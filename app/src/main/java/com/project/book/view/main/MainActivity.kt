d6a8db4bc91875900bd5857cef605861163b847c
정민혁 <jlssteeve1@gmail.com>
2022-03-11 15:05:27 +0900

    Add toolbar transaction

--- app/src/main/java/com/project/book/view/main/MainActivity.kt
+++ app/src/main/java/com/project/book/view/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.project.book.view.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.project.book.R
@@ -8,8 +10,8 @@ import com.project.book.base.BaseActivity
 import com.project.book.databinding.ActivityMainBinding
 import com.project.book.view.home.HomeFragment
 import com.project.book.view.profile.ProfileFragment
+import com.project.book.view.search.SearchActivity
 import com.project.book.view.settings.SettingsFragment
-
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
@@ -22,6 +24,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         setContentView(binding.root)
 
         initNavigationView()
+        onMenuClicked()
     }
 
     private fun initNavigationView() {
@@ -41,6 +44,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 true
             }
             selectedItemId = R.id.my_home
+        }
+    }
+    private fun onMenuClicked(){
+        binding.toolBar.title = "책좀 보자"
+        binding.toolBar.setOnMenuItemClickListener{
+            when(it.itemId){
+                R.id.tool_search->{
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    Log.d("MainActivity","search clicked")
+                    true
+                }
+                R.id.tool_home->{
+                    changeFragment(homeFragment)
+                    Log.d("MainActivity","home clicked")
+                    true
+                }
+                R.id.tool_profile->{
+                    changeFragment(profileFragment)
+                    Log.d("MainActivity","profile clicked")
+                    true
+                }
+                R.id.tool_settings->{
+                    changeFragment(settingsFragment)
+                    Log.d("MainActivity","settings clicked")
+                    true
+                }
+                else -> false
+            }
         }
     }
 
