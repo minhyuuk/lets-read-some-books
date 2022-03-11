d8980c8fa937e08f88a34e849cfa527a1cba357e
정민혁 <jlssteeve1@gmail.com>
2022-03-11 14:13:06 +0900

    remove code

--- app/src/main/java/com/project/book/util/Constants.kt
+++ app/src/main/java/com/project/book/util/Constants.kt
@@ -1,5 +1,8 @@
 package com.project.book.util
 
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 object API {
     const val BASE_URL : String = "https://book.interpark.com/api/"
     const val KEY : String = "765A3F027389533B4C069DA9B269D806EDDB5CC1431CD8A053BB6FA19D6C1642"
--- app/src/main/java/com/project/book/util/Extensions.kt
+++ app/src/main/java/com/project/book/util/Extensions.kt
@@ -1,7 +1,14 @@
 package com.project.book.util
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.project.book.R
+import com.project.book.view.main.MainActivity
 
 object Extensions {
     fun Activity.toast(msg: String){
