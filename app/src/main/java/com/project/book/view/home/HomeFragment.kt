3ec431feebb4d2acd9368512cbc27b5077fcc6df
정민혁 <jlssteeve1@gmail.com>
2022-03-11 15:48:14 +0900

    Fix toolbar fragment to activity

--- app/src/main/java/com/project/book/view/home/HomeFragment.kt
+++ app/src/main/java/com/project/book/view/home/HomeFragment.kt
@@ -1,38 +1,31 @@
 package com.project.book.view.home
 
-import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Bundle
 import android.util.Log
+import android.view.*
+import androidx.fragment.app.Fragment
 import com.project.book.R
 import com.project.book.base.BaseFragment
+import com.project.book.databinding.ActivityMainBinding
 import com.project.book.databinding.FragmentHomeBinding
+import com.project.book.view.main.MainActivity
 import com.project.book.view.profile.ProfileFragment
 import com.project.book.view.search.SearchActivity
 import com.project.book.view.settings.SettingsFragment
-import kotlin.concurrent.fixedRateTimer
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    override fun FragmentHomeBinding.onViewCreated() {
-        binding.profileImg.setOnClickListener {
-            fragmentManager
-                ?.beginTransaction()
-                ?.replace(R.id.fl_container,ProfileFragment())
-                ?.commit()
-        }
-        binding.searchImg.setOnClickListener {
-            val intent = Intent(activity,SearchActivity::class.java)
-            startActivity(intent)
-        }
-        binding.settingsImg.setOnClickListener {
-            fragmentManager
-                ?.beginTransaction()
-                ?.replace(R.id.fl_container,SettingsFragment())
-                ?.commit()
-        }
+    private val profileFragment by lazy { ProfileFragment() }
+    private val settingsFragment by lazy { SettingsFragment() }
+
+    override fun FragmentHomeBinding.onViewCreated() {}
+    override fun FragmentHomeBinding.onCreateView() {}
+
+    private fun changeFragment(fragment: Fragment) {
+        (activity as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_container, fragment).commit()
     }
 
-    override fun FragmentHomeBinding.onCreateView() {
 
-    }
 }
